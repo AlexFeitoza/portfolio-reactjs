6be3b09c777411888778b29fd78c84a200e0f02e
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2025-01-12 08:52:45 -0300

    commit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,102 +3,80 @@
  * @license Apache-2.0
  */
 
-
 import Skillcard from "./Skillcard";
 
 const skillItem = [
-    {
-      imgSrc: '/images/git-logo.svg',
-      label: 'Git',
-      desc: 'Versionamento de Código'
-    },
-    {
-      imgSrc: '/images/css3.svg',
-      label: 'CSS',
-      desc: 'User Interface'
-    },
-    {
-      imgSrc: '/images/javascript.svg',
-      label: 'JavaScript',
-      desc: 'Interação'
-    },
-    {
-      imgSrc: '/images/nodejs.svg',
-      label: 'NodeJS',
-      desc: 'Web Server'
-    },
-    {
-      imgSrc: '/images/expressjs.svg',
-      label: 'ExpressJS',
-      desc: 'Node Framework'
-    },
-    {
-      imgSrc: '/images/mongodb-icon.svg',
-      label: 'MongoDB',
-      desc: 'Banco de Dados'
-    },
-    {
-      imgSrc: '/images/react.svg',
-      label: 'React',
-      desc: 'Framework'
-    },
-    {
-      imgSrc: '/images/tailwindcss.svg',
-      label: 'TailwindCSS',
-      desc: 'User Interface'
-    },
-    
-    {
-      imgSrc: '/images/prisma-icon.svg',
-      label: 'Prisma',
-      desc: 'ORM'
-    },
-    {
-      imgSrc: '/images/styledcomponents-icon.svg',
-      label: 'Styled Components',
-      desc: 'User Interface'
-    },
+  {
+    imgSrc: "/images/git-logo.svg",
+    label: "Git",
+    desc: "Versionamento de Código",
+  },
+  {
+    imgSrc: "/images/css3.svg",
+    label: "CSS",
+    desc: "User Interface",
+  },
+  {
+    imgSrc: "/images/javascript.svg",
+    label: "JavaScript",
+    desc: "Interação",
+  },
+  {
+    imgSrc: "/images/nodejs.svg",
+    label: "NodeJS",
+    desc: "Web Server",
+  },
+  {
+    imgSrc: "/images/expressjs.svg",
+    label: "ExpressJS",
+    desc: "Node Framework",
+  },
+  {
+    imgSrc: "/images/mongodb-icon.svg",
+    label: "MongoDB",
+    desc: "Banco de Dados",
+  },
+  {
+    imgSrc: "/images/react.svg",
+    label: "React",
+    desc: "Framework",
+  },
+  {
+    imgSrc: "/images/tailwindcss.svg",
+    label: "TailwindCSS",
+    desc: "User Interface",
+  },
 
-  ];
-
+  {
+    imgSrc: "/images/prisma-icon.svg",
+    label: "Prisma",
+    desc: "ORM",
+  },
+  {
+    imgSrc: "/images/styledcomponents-icon.svg",
+    label: "Styled Components",
+    desc: "User Interface",
+  },
+];
 
 const Skills = () => {
   return (
     <section className="section">
-        <div className="container">
+      <div className="container">
+        <h2 className="headline-2">Tecnologias e Ferramentas</h2>
 
-            <h2 className="headline-2">
-              Tecnologias e Ferramentas
-            </h2>
+        <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch]">
+          Ferramentas e tecnologias que utilizo para criar sites e aplicativos excepcionais e de alto desempenho.
+        </p>
 
-            <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch]">
-<<<<<<< Updated upstream
-            Poderosas ferramentas e tecnologias que utilizo para criar sites e aplicativos excepcionais e de alto desempenho.
-=======
-<<<<<<< HEAD
-            Ferramentas e tecnologias poderosas que eu utilizo para criar sites e aplicações exclusivas e de alto desempenho.
-
-=======
-            Poderosas ferramentas e tecnologias que utilizo para criar sites e aplicativos excepcionais e de alto desempenho.
->>>>>>> 815306a6d8abe0534c20a648e727a9574a60f4a9
->>>>>>> Stashed changes
-            </p>
-
-            <div className="grid gap-3 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]">
-                {
-                    skillItem.map(({imgSrc, label, desc}, key) => (
-                        <Skillcard 
-                        key={key}
-                        imgSrc={imgSrc}
-                        label={label}
-                        desc={desc}/>
-                    ))
-                }
-            </div>
-
+        <div className="grid gap-3 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]">
+          {skillItem.map(({ imgSrc, label, desc }, key) => (
+            <Skillcard key={key} imgSrc={imgSrc} label={label} desc={desc} />
+          ))}
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
